e723e86be392d44d1f3e5f4359e04b1feaeee933
ryan-waterloo <r4yen@uwaterloo.ca>
2024-08-30 20:15:40 -0400

    add bibtex

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@
 	$("#owl-slider").owlCarousel({
 		navigation: false,
 		pagination: true,
-		loop:true,
+		loop: true,
 		itemsCustom: [
 			[0, 1],
 			[960, 2],
@@ -250,14 +250,87 @@
 	/*---------------------------------------------------- */
 	/*	Expand news
 	  ------------------------------------------------------*/
-	$('.news-box-content').on('click', function() {
+	$('.news-box-content').on('click', function () {
 		// toggle the class
 		$(this).toggleClass('news-box-content-expand');
 
 		// change the text of .news-toggle, a button as the child of .news-box-content
-		$(this).children('.news-toggle').text(function(i, text) {
+		$(this).children('.news-toggle').text(function (i, text) {
 			return text === "Read More..." ? "Read Less..." : "Read More...";
 		});
+	});
+
+	var originalTexts = {
+		introParagraph1: $('#intro-paragraph1').html(),
+		introParagraph2: $('#intro-paragraph2').html(),
+		introParagraph3: $('#intro-paragraph3').html(),
+		// Add more original texts for other paragraphs if needed
+	};
+	$('.bibtex-link').on('click', function (event) {
+		event.preventDefault();
+		var bibtexId = $(this).data('bibtex-id');
+		var introParagraph = $('#intro-paragraph' + bibtexId.slice(-1));
+
+		var bibtexEntries = {
+			bibtex1: `@inproceedings{10.1145/3672539.3686324,
+title={Code Shaping: Iterative Code Editing with Free-form Sketching},
+author={Yen, Ryan and Zhao, Jian and Vogel, Daniel},
+booktitle={Proceedings of the Conference on Human Factors in Computing Systems},
+year={2024},
+publisher={Association for Computing Machinery},
+isbn={9790400707186},
+address={New York, NY, USA},
+url={https://doi.org/10.1145/3672539.3686324},
+doi={10.1145/3672539.3686324},
+series={UIST Adjunct '24}
+}`,
+			bibtex2: `@inproceedings{10.1145/3654777.3676357,
+author = {Yen, Ryan and Zhu, Jiawen and Suh, Sangho and Xia, Haijun and Zhao, Jian},
+title = {CoLadder: Manipulating Code Generation via Multi-Level Blocks},
+year = {2024},
+isbn = {9798400706288},
+publisher = {Association for Computing Machinery},
+address = {New York, NY, USA},
+url = {https://doi.org/10.1145/3654777.3676357},
+doi = {10.1145/3654777.3676357},
+booktitle = {Proceedings of the 37th Annual ACM Symposium on User Interface Software and Technology},
+keywords = {hierarchical code generation, multi-level abstraction, programming support},
+location = {Pittsburgh, PA, USA},
+series = {UIST '24}
+}`,
+			bibtex3: `@inproceedings{10.1145/3654777.3676388,
+author = {Yen, Ryan and Zhao, Jian},
+title = {Memolet: Reifying the Reuse of User-AI Conversational Memories},
+year = {2024},
+isbn = {9798400706288},
+publisher = {Association for Computing Machinery},
+address = {New York, NY, USA},
+url = {https://doi.org/10.1145/3654777.3676388},
+doi = {10.1145/3654777.3676388},
+booktitle = {Proceedings of the 37th Annual ACM Symposium on User Interface Software and Technology},
+articleno = {1},
+numpages = {12},
+keywords = {memory management, knowledge reuse, conversational agent, human-AI system},
+location = {Pittsburgh, PA, USA},
+series = {UIST '24}
+}`,
+			// Add more BibTeX entries here for other publications
+		};
+
+		var bibtexEntry = bibtexEntries[bibtexId];
+
+		// Toggle between original text and BibTeX entry
+		if (introParagraph.html().includes('@inproceedings')) {
+			introParagraph.html(originalTexts['introParagraph' + bibtexId.slice(-1)]);
+		} else {
+			introParagraph.html('<pre>' + bibtexEntry + '</pre>');
+
+			// Copy to clipboard
+			var tempTextarea = $('<textarea>');
+			tempTextarea.val(bibtexEntry).appendTo('body').select();
+			document.execCommand('copy');
+			tempTextarea.remove();
+		}
 	});
 
 })(jQuery);
